214d45151922a97cb47f2ff2c2ba291ff5841ee3
jonas <jonas-gruempel@gmx.de>
2019-01-12 22:57:33 +0100

    update (added inline-documentation, is unable to handle submenu yet)

--- customContextMenu.js
+++ customContextMenu.js
@@ -1,10 +1,14 @@
 	const customContextMenu = {
 		content: [],
+		submenus: [],
 		container: null,
 		clickedElement: null,
-		width: '',
-		height: '',
+		width: 0,
+		height: 0,
 		id: '',
+		/**
+		 * setting up the enviromental context menu values
+		 */
 		setup: function () {
 			this.setupListener();
 			this.container = document.createElement('div');
@@ -22,6 +26,9 @@
 				throw new Error('you need to have a html tag in your document');
 			}
 		},
+		/**
+		 * generates an unique id (internal use)
+		 */
 		generateId: function () {
 		  function s4() {
 			return Math.floor((1 + Math.random()) * 0x10000)
@@ -30,30 +37,40 @@
 		  }
 		  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 		},
+		/**
+		 * setting up all global event listeners
+		 */
 		setupListener: function () {
 			document.addEventListener('contextmenu', ev => {
 				this.clickedElement = ev.path[0];
 				this.hide();
 				this.show();
-				let top = ev.clientY + 'px';
-				let left = ev.clientX + 'px';
+				let top = this.calculateYOpenLocation( ev.clientY ) + 'px';
+				let left = this.calculateXOpenLocation( ev.clientX ) + 'px';
 				this.container.style.top = top;
 				this.container.style.left = left;
 				ev.preventDefault();
 			}); 
-			document.addEventListener('click', ev => {
+			document.addEventListener('click', _ => {
 				this.hide();
 			});
-			window.addEventListener('blur', ev => {
+			window.addEventListener('blur', _ => {
 				this.hide();
 			});
-			window.addEventListener('focus', ev => {
+			window.addEventListener('focus', _ => {
 				this.hide();
 			});
-			window.addEventListener('keyup', ev => {
+			window.addEventListener('keyup', _ => {
 				this.hide();
 			}); 
+			window.addEventListener('resize', _ => {
+				this.hide();
+			});
 		},
+		/**
+		 * adding an elemnt to context menu
+		 * @param {object} el context menu element
+		 */
 		addElement: function (el) {
 			let type = el.type;
 			if (type != undefined) {
@@ -81,6 +98,11 @@
 				throw new Error('You have to enter a type definition of context menu item');
 			}
 		},
+		/**
+		 * creates a context menu button element
+		 * @param {string} text text of button 
+		 * @param {function} click function which will be executed after a click
+		 */
 		createButtonElement: function (text, click) {
 			let btn = document.createElement('button');
 			btn.innerHTML = text;
@@ -91,14 +113,22 @@
 			});
 			return btn;
 		},
+		/**
+		 * creates a context menu seperator element
+		 */
 		createSeperatorElement: function (){
 			let sep = document.createElement('div');
 			sep.className = 'ctxSeperator';
 			return sep;
 		},
+		//
 		createSubmenu: function (text, content) {
 
 		},
+		/**
+		 * setting up a context menu using a template object
+		 * @param {object} tem context menu template
+		 */
 		buildFromTemplate: function (tem) {
 			if (Array.isArray(tem)) {
 				this.content = [];
@@ -113,6 +143,37 @@
 				throw new Error('You have to enter an array');
 			}
 		},
+		/**
+		 * calculates the used x position
+		 * @param {number} mouseX mouse x position 
+		 */
+		calculateXOpenLocation: function (mouseX) {
+			var x;
+			let wWidth = window.innerWidth;
+			if (wWidth < (mouseX + this.width)) {
+				x =  wWidth - this.width;
+			} else {
+				x = mouseX;
+			}
+			return x;
+		},
+		/**
+		 * calculates the used y position
+		 * @param {number} mouseY mouse y position 
+		 */
+		calculateYOpenLocation: function (mouseY) {
+			var y;
+			let wWidth = window.innerHeight;
+			if (wWidth < (mouseY + this.height)) {
+				y = mouseY - this.height;
+			} else {
+				y = mouseY;
+			}
+			return y;
+		},
+		/**
+		 * setting up the html elements in context menu
+		 */
 		setupHTMLElements: function () {
 			if (this.container != null) {
 				if (this.content.length != this.container.children.length) {
@@ -122,14 +183,22 @@
 				}
 			}
 		},
+		/**
+		 * shows the context menu
+		 */
 		show: function () {
 			this.container.className = 'ctxContainer';
 			this.setupHTMLElements();
+			this.width = this.container.offsetWidth;
+			this.height = this.container.offsetHeight;
 		},
+		/**
+		 * hides the context menu
+		 */
 		hide: function () {
 			if (this.container != null) {
+				this.hideAllSubmenus();
 				this.container.className = 'ctxContainerHidden';
-				//this.container.innerHTML = null;
 			}
 		}
 	}
@@ -165,6 +234,40 @@
 			text: 'button 4',
 			click: c => {
 				alert('button 4');
+			}
+		},
+		{
+			type: 'seperator'
+		},
+		{
+			type: 'button',
+			text: 'button 5',
+			click: c => {
+				alert('button 5');
+			}
+		},
+		{
+			type: 'button',
+			text: 'button 6',
+			click: c => {
+				alert('button 6');
+			}
+		},
+		{
+			type: 'seperator'
+		},
+		{
+			type: 'button',
+			text: 'button 7',
+			click: c => {
+				alert('button 7');
+			}
+		},
+		{
+			type: 'button',
+			text: 'button 8',
+			click: c => {
+				alert('button 8');
 			}
 		}
 	]);
